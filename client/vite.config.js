bfff296d3472c8266261d204642400282e658af7
George <152157371+George15526@users.noreply.github.com>
2025-05-21 16:20:04 +0800

    Update Vite server configuration to set host to true and ensure allowedHosts is enabled

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: true,
     strictPort: true,
     port: 5173,
     proxy: {
       '/api': 'http://localhost:5000'
-    }
+    },
+    allowedHosts: true
   }
 })
